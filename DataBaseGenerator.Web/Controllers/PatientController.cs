7ffd79ca0fe45c9936c7059d884fce8f79138cde
ADANI\zabrodin <rediskal50@gmail.com>
2025-07-28 14:15:59 +0300

    ref: add Controllers Model and viewmodels

---
+++ DataBaseGenerator.Web/Controllers/PatientController.cs
@@ -0,0 +1,40 @@
+﻿using DataBaseGenerator.Core;
+using DataBaseGenerator.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataBaseGenerator.Web.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly IPatientService _patientService;
+
+        public PatientController(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
+
+        public IActionResult Index()
+        {
+            var patient = _patientService.GetAll();
+
+            if (!patient.Any())
+                ViewBag.Message = "Пациенты в базе отсутствуют.";
+
+            var viewmodel = patient.Select(p => new PatientViewModel
+            {
+                Id = p.ID_Patient,
+                LastName = p.LastName,
+                FirstName = p.FirstName,
+                MiddleName = p.MiddleName,
+                BirthDate = p.BirthDate,
+                Sex = p.Sex,
+                Address = p.Address,
+                AddInfo = p.AddInfo,
+                Occupation = p.Occupation
+            });
+
+            return View(viewmodel);
+        }
+
+    }
+}
