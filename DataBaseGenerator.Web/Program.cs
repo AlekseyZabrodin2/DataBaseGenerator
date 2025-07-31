4cea4e71273995b24457e7847fa247bbf0010d88
ADANI\zabrodin <rediskal50@gmail.com>
2025-07-31 12:19:18 +0300

    ref: add tabs and change start method

--- DataBaseGenerator.Web/Program.cs
+++ DataBaseGenerator.Web/Program.cs
@@ -39,14 +39,9 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}")
-//    .WithStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Patient}/{action=Index}/{id?}");
-//.WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 app.Run();
