599712842309a2472fb1707df6e0380d01664ba2
ADANI\zabrodin <rediskal50@gmail.com>
2025-08-06 11:08:28 +0300

    feat (web): create services for Api

---
+++ DataBaseGenerator.Web/Services/PatientService.cs
@@ -0,0 +1,123 @@
+﻿using System.Threading.Tasks;
+using DataBaseGenerator.Core;
+using DataBaseGenerator.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseGenerator.Web.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly BaseGenerateContext _context;
+        private Patient _patient;
+        private DbContextOptions<BaseGenerateContext> _options = new DbContextOptionsBuilder<BaseGenerateContext>()
+            .UseMySql("server = localhost; database = medxregistry; user = root; password = root; port = 3306"
+            , new MySqlServerVersion(new Version(8, 0, 28))).Options;
+
+
+        public PatientService(BaseGenerateContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<Patient>> GetAllAsync()
+        {
+            return await _context.Patient.ToListAsync();
+        }
+
+        public async Task GenerateAsync(PatientGeneratorDto inputParameters)
+        {
+            for (var patientindex = 0; patientindex < inputParameters.PatientCount; patientindex++)
+            {
+               await CreateAsync(patientindex, inputParameters);
+            }
+        }
+
+        public async Task CreateAsync(int patientIndex, PatientGeneratorDto patientGeneratorParameters)
+        {
+            bool checkIsExist = _context.Patient.Any(element => element.ID_Patient == patientGeneratorParameters.ID_Patient.Generate(patientIndex));
+
+            if (checkIsExist)
+                return;
+
+            _patient = GeneratePatients(patientIndex, patientGeneratorParameters);
+            _context.Patient.Add(_patient);
+            await _context.SaveChangesAsync();
+        }
+
+        private Patient GeneratePatients(int patientIndex, PatientGeneratorDto patientGeneratorParameters)
+        {
+            return new Patient
+            {
+                ID_Patient = patientGeneratorParameters.ID_Patient.Generate(patientIndex),
+                LastName = patientGeneratorParameters.LastName.Generate(),
+                FirstName = patientGeneratorParameters.FirstName.Generate(),
+                MiddleName = patientGeneratorParameters.MiddleName.Generate(),
+                PatientID = patientGeneratorParameters.PatientID.Generate(patientIndex),
+                BirthDate = patientGeneratorParameters.BirthDate.Generate(),
+                Sex = patientGeneratorParameters.Sex.Generate(),
+                Address = patientGeneratorParameters.Address.Generate(),
+                AddInfo = patientGeneratorParameters.AddInfo.Generate(),
+                Occupation = patientGeneratorParameters.Occupation.Generate()
+            };
+        }
+
+        public async Task AddOneAsync(PatientInputParameters inputParameters)
+        {
+            await CreateOne(inputParameters);
+        }
+
+        public async Task CreateOne(PatientInputParameters patientGeneratorParameters)
+        {
+            bool checkIsExist = _context.Patient.Any(element => element.PatientID == patientGeneratorParameters.PatientID);
+
+            if (checkIsExist)
+                return;
+
+            _patient = CreateOnePatient(patientGeneratorParameters);
+            _context.Patient.Add(_patient);
+            await _context.SaveChangesAsync();
+        }
+
+        private Patient CreateOnePatient(PatientInputParameters patientGeneratorParameters)
+        {
+            return new Patient
+            {
+                ID_Patient = patientGeneratorParameters.ID_Patient,
+                LastName = patientGeneratorParameters.LastName,
+                FirstName = patientGeneratorParameters.FirstName,
+                MiddleName = patientGeneratorParameters.MiddleName,
+                PatientID = patientGeneratorParameters.PatientID,
+                BirthDate = patientGeneratorParameters.BirthDate,
+                Sex = patientGeneratorParameters.Sex,
+                Address = patientGeneratorParameters.Address,
+                AddInfo = patientGeneratorParameters.AddInfo,
+                Occupation = patientGeneratorParameters.Occupation
+            };
+        }
+
+        public async Task DeleteFirstAsync()
+        {
+            _context.Patient.Remove(_context.Patient.First());
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAllAsync()
+        {
+            _context.Patient.RemoveRange(_context.Patient);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditeAsync(Patient oldPatient, int iD, string lastName, string name)
+        {
+            Patient patient = _context.Patient.FirstOrDefault(position => position.ID_Patient == oldPatient.ID_Patient);
+            if (patient != null)
+            {
+                patient.ID_Patient = iD;
+                patient.LastName = lastName;
+                patient.FirstName = name;
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ DataBaseGenerator.Web/Services/WorklistService.cs
@@ -0,0 +1,94 @@
+﻿using DataBaseGenerator.Core;
+using DataBaseGenerator.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseGenerator.Web.Services
+{
+    public class WorklistService : IWorklistService
+    {
+        private readonly BaseGenerateContext _context;
+        private WorkList _newWorkList;
+        private DbContextOptions<BaseGenerateContext> _options = new DbContextOptionsBuilder<BaseGenerateContext>()
+            .UseMySql("server = localhost; database = medxregistry; user = root; password = root; port = 3306"
+            , new MySqlServerVersion(new Version(8, 0, 28))).Options;
+
+
+        public WorklistService(BaseGenerateContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<WorkList>> GetAllAsync()
+        {
+            return await _context.WorkList.ToListAsync();
+        }
+
+        public async Task GenerateAsync(WorkListGeneratorDto inputParameters)
+        {
+            for (var workListIndex = 0; workListIndex < inputParameters.WorkListCount; workListIndex++)
+            {
+                using var context = new BaseGenerateContext(_options);
+                await CreateAsync(workListIndex, inputParameters, context);
+            }
+        }
+
+        public async Task CreateAsync(int workListIndex, WorkListGeneratorDto inputParameters, BaseGenerateContext context)
+        {
+            _newWorkList = GenerateWorkList(workListIndex, inputParameters);
+
+            bool checkIsExist = context.WorkList.Any(
+                     workList => workList.ID_WorkList == inputParameters.ID_WorkList.Generate(workListIndex) &&
+                                 workList.CreateDate == inputParameters.CreateDate.Generate() &&
+                                 workList.CreateTime == inputParameters.CreateTime.Generate() &&
+                                 workList.ID_Patient == inputParameters.ID_Patient.Generate(workListIndex) &&
+                                 workList.State == inputParameters.State.Generate() &&
+                                 workList.SOPInstanceUID == inputParameters.SOPInstanceUID.Generate() &&
+                                 workList.Modality == inputParameters.Modality.Generate() &&
+                                 workList.StationAeTitle == inputParameters.StationAeTitle.Generate() &&
+                                 workList.ProcedureStepStartDateTime == inputParameters.ProcedureStepStartDateTime.Generate() &&
+                                 workList.PerformingPhysiciansName == inputParameters.PerformingPhysiciansName.Generate() &&
+                                 workList.StudyDescription == inputParameters.StudyDescription.Generate() &&
+                                 workList.ReferringPhysiciansName == inputParameters.ReferringPhysiciansName.Generate() &&
+                                 workList.RequestingPhysician == inputParameters.RequestingPhysician.Generate());
+
+            if (checkIsExist)
+                return;
+
+            context.WorkList.Add(_newWorkList);
+            await context.SaveChangesAsync();
+        }
+
+        private WorkList GenerateWorkList(int workListIndex, WorkListGeneratorDto inputParameters)
+        {
+            return new WorkList()
+            {
+                ID_WorkList = inputParameters.ID_WorkList.Generate(workListIndex),
+                CreateDate = inputParameters.CreateDate.Generate(),
+                CreateTime = inputParameters.CreateTime.Generate(),
+                ID_Patient = inputParameters.ID_Patient.Generate(workListIndex),
+                State = inputParameters.State.Generate(),
+                SOPInstanceUID = inputParameters.SOPInstanceUID.Generate(),
+                Modality = inputParameters.Modality.Generate(),
+                StationAeTitle = inputParameters.StationAeTitle.Generate(),
+                ProcedureStepStartDateTime = inputParameters.ProcedureStepStartDateTime.Generate(),
+                PerformingPhysiciansName = inputParameters.PerformingPhysiciansName.Generate(),
+                StudyDescription = inputParameters.StudyDescription.Generate(),
+                ReferringPhysiciansName = inputParameters.ReferringPhysiciansName.Generate(),
+                RequestingPhysician = inputParameters.RequestingPhysician.Generate()
+            };
+        }
+
+        public async Task DeleteFirstAsync()
+        {
+            _context.WorkList.Remove(_context.WorkList.First());
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAllAsync()
+        {
+            _context.WorkList.RemoveRange(_context.WorkList);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
