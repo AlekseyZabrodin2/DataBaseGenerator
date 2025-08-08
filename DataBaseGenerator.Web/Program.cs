0b3da266b5ed356fa1a2f90abe6752f4ba640677
ADANI\zabrodin <rediskal50@gmail.com>
2025-08-08 12:32:42 +0300

    ref (web): refactoring program meyhod, add logger and stop process method

--- DataBaseGenerator.Web/Program.cs
+++ DataBaseGenerator.Web/Program.cs
@@ -1,63 +1,105 @@
+using System.Diagnostics;
 using System.Net.Http.Headers;
 using DataBaseGenerator.Core.Data;
 using DataBaseGenerator.Web.Controllers.ApiControllers;
 using DataBaseGenerator.Web.Services;
 using Microsoft.EntityFrameworkCore;
+using NLog;
 
-var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://localhost:5289");
-//builder.WebHost.UseUrls("http://localhost:5289", "https://localhost:7168");
 
-builder.Configuration.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.json"));
+var logger = LogManager.Setup().LoadConfigurationFromFile().GetCurrentClassLogger();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var processName = Process.GetCurrentProcess().ProcessName;
+var existingProcesses = Process.GetProcessesByName(processName)
+                               .Where(p => p.Id != Process.GetCurrentProcess().Id);
 
-builder.Services.AddDbContext<BaseGenerateContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 28))
-    ));
-
-builder.Services.AddScoped<IPatientService, PatientService>();
-builder.Services.AddScoped<IWorklistService, WorklistService>();
-builder.Services.AddScoped<PatientApiController>();
-
-builder.Services.AddHttpClient("DBGeneratorApi", client =>
+if (existingProcesses.Any())
 {
-    client.BaseAddress = new Uri("http://localhost:5289/api/");
-    client.DefaultRequestHeaders.Accept.Clear();
-    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-});
+    logger.Info($"Found {existingProcesses.Count()} other instance(s) of {processName}, stopping them...");
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    foreach (var process in existingProcesses)
+    {
+        try
+        {
+            process.Kill();
+            process.WaitForExit();
+            logger.Info($"Process {process.Id} stopped successfully.");
+        }
+        catch (Exception ex)
+        {
+            logger.Warn(ex, $"Failed to stop process {process.Id}");
+        }
+    }
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    logger.Info("All previous web host instances stopped. Starting new instance...");
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
+try
+{
+    logger.Trace("Initialization WebApplication");
 
-app.UseStaticFiles();
-//app.UseHttpsRedirection();
-app.UseRouting();
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseAuthorization();
+    builder.WebHost.UseUrls("http://localhost:5289");
+    //builder.WebHost.UseUrls("http://localhost:5289", "https://localhost:7168");
 
-app.MapStaticAssets();
+    builder.Configuration.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.json"));
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-app.Run();
+    builder.Services.AddDbContext<BaseGenerateContext>(options =>
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("DefaultConnection"),
+            new MySqlServerVersion(new Version(8, 0, 28))
+        ));
+
+    builder.Services.AddScoped<IPatientService, PatientService>();
+    builder.Services.AddScoped<IWorklistService, WorklistService>();
+    builder.Services.AddScoped<PatientApiController>();
+
+    builder.Services.AddHttpClient("DBGeneratorApi", client =>
+    {
+        client.BaseAddress = new Uri("http://localhost:5289/api/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    });
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseStaticFiles();
+    //app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    logger.Info("WebHost is started");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    logger.Fatal(ex, "WebHost can`t started");
+}
