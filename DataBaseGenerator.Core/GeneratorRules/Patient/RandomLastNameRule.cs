7645f73574139c7ed48086edb6cae3d6621b1ebf
ADANI\zabrodin <rediskal50@gmail.com>
2025-08-19 16:39:25 +0300

    ref (core): add english and chinese LastName dictionary

--- DataBaseGenerator.Core/GeneratorRules/Patient/RandomLastNameRule.cs
+++ DataBaseGenerator.Core/GeneratorRules/Patient/RandomLastNameRule.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataBaseGenerator.Core.GeneratorRules.Patient
 {
     public sealed class RandomLastNameRule : IGeneratorRule<string>
     {
+        private readonly Random _random = new();
+
         private static readonly IDictionary<int, string> _russianLastName = new Dictionary<int, string>
         {
             {0, "Покровский"},
@@ -21,14 +20,70 @@ namespace DataBaseGenerator.Core.GeneratorRules.Patient
             {8, "Чацкий"},
             {9, "Ростов"}
         };
-        
-        public string Generate()
+
+        private static readonly IDictionary<int, string> _englishLastNames = new Dictionary<int, string>
         {
-            var random = new Random();
+            {0, "Smith"},
+            {1, "Johnson"},
+            {2, "Williams"},
+            {3, "Brown"},
+            {4, "Jones"},
+            {5, "Miller"},
+            {6, "Davis"},
+            {7, "Garcia"},
+            {8, "Rodriguez"},
+            {9, "Wilson"}
+        };
 
-            var lastName = _russianLastName[random.Next(0, _russianLastName.Count)];
+        private static readonly IDictionary<int, string> _chineseLastNames = new Dictionary<int, string>
+        {
+            {0, "王 (Wang)"},
+            {1, "李 (Li)"},
+            {2, "张 (Zhang)"},
+            {3, "刘 (Liu)"},
+            {4, "陈 (Chen)"},
+            {5, "杨 (Yang)"},
+            {6, "赵 (Zhao)"},
+            {7, "黄 (Huang)"},
+            {8, "周 (Zhou)"},
+            {9, "吴 (Wu)"}
+        };
 
-            return lastName;
+        public string Generate() => GenerateRussian();
+
+        public string GenerateRussian()
+        {
+            return _russianLastName[_random.Next(0, _russianLastName.Count)];
+        }
+
+        public string GenerateEnglish()
+        {
+            return _englishLastNames[_random.Next(0, _englishLastNames.Count)];
+        }
+
+        public string GenerateChinese()
+        {
+            return _chineseLastNames[_random.Next(0, _chineseLastNames.Count)];
+        }
+
+        public string GenerateLastName(PatientGeneratorDto patientGenerator)
+        {
+            var generatorLastNames = new List<Func<string>>();
+
+            if (patientGenerator.NamesRusGeneratorRule)
+                generatorLastNames.Add(GenerateRussian);
+
+            if (patientGenerator.NamesEngGeneratorRule)
+                generatorLastNames.Add(GenerateEnglish);
+
+            if (patientGenerator.NamesChinaGeneratorRule)
+                generatorLastNames.Add(GenerateChinese);
+
+            if (generatorLastNames.Count == 0)
+                return Generate();
+
+            var lastName = generatorLastNames[_random.Next(generatorLastNames.Count)];
+            return lastName();
         }
 
         public override string ToString()
