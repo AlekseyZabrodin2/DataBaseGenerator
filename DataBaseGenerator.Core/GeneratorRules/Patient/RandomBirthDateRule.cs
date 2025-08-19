40ab2d2df73a42f320a74122273d3768b152be54
ADANI\zabrodin <rediskal50@gmail.com>
2025-08-19 16:44:07 +0300

    ref (core): add methods for Generate Birthdate by radiobuttons

--- DataBaseGenerator.Core/GeneratorRules/Patient/RandomBirthDateRule.cs
+++ DataBaseGenerator.Core/GeneratorRules/Patient/RandomBirthDateRule.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace DataBaseGenerator.Core.GeneratorRules.Patient
 {
-    public sealed class RandomBirthDateRule : IGeneratorRule<DateTime>
+    public sealed class RandomBirthDateRule : IGeneratorRule<DateTime?>
     {
+        private readonly Random _random = new();
+        private readonly DateTime _nowDate = DateTime.Now;
+
+        private int _randomMonth = 0;
+        private int _randomDay = 0;
+
         public RandomBirthDateRule(DateTime birthDate)
         {
             BirthDate = birthDate;
@@ -15,19 +21,56 @@ namespace DataBaseGenerator.Core.GeneratorRules.Patient
 
         public DateTime BirthDate { get; }
 
-        public DateTime Generate()
+        private DateTime CreateDate(int year)
         {
-            var random = new Random();
+            _randomMonth = _random.Next(1, 13);
+            _randomDay = _random.Next(1, DateTime.DaysInMonth(year, _randomMonth) + 1);
 
-            DateTime nowDate = DateTime.Now;
+            return new DateTime(year, _randomMonth, _randomDay);
+        }
 
-            int rndYear = random.Next(1950, nowDate.Year);
-            int rndMonth = random.Next(1, 12);
-            int rndDay = random.Next(1, 28);
+        public DateTime? Generate() => CreateDate(_random.Next(1950, _nowDate.Year));
 
-            var birthDate = new DateTime(rndYear, rndMonth, rndDay);
+        public DateTime? GenerateMissingBirthdate() => null;
 
-            return birthDate;
+        public DateTime? GenerateFutureBirthdate() => CreateDate(_random.Next(_nowDate.Year, _nowDate.Year + 100));
+
+        public DateTime? GenerateAgeFrom0To17() => CreateDate(_random.Next(_nowDate.Year - 17, _nowDate.Year));
+
+        public DateTime? GenerateAgeFrom18To60() => CreateDate(_random.Next(_nowDate.Year - 60, _nowDate.Year - 18));
+
+        public DateTime? GenerateAgeFrom61To120() => CreateDate(_random.Next(_nowDate.Year - 120, _nowDate.Year - 61));
+
+
+        public DateTime? GenerateBirthdate(PatientGeneratorDto patientGenerator)
+        {
+            var generatorBirthdate = new List<Func<DateTime?>>();
+
+            if (patientGenerator.RandomBirthdateGeneratorRule)
+                generatorBirthdate.Add(Generate);
+
+            if (patientGenerator.MissingBirthdateGeneratorRule)
+                generatorBirthdate.Add(GenerateMissingBirthdate);
+
+            if (patientGenerator.FutureBirthdateGeneratorRule)
+                generatorBirthdate.Add(GenerateFutureBirthdate);
+
+            if (patientGenerator.Age0_17_GeneratorRule)
+                generatorBirthdate.Add(GenerateAgeFrom0To17);
+
+            if (patientGenerator.Age18_60_GeneratorRule)
+                generatorBirthdate.Add(GenerateAgeFrom18To60);
+
+            if (patientGenerator.Age61_120_GeneratorRule)
+                generatorBirthdate.Add(GenerateAgeFrom61To120);
+
+            if (generatorBirthdate.Count == 0)
+                return _nowDate;
+
+            var birthdate = generatorBirthdate[_random.Next(generatorBirthdate.Count)];
+            var result = birthdate();
+
+            return result;
         }
 
         public override string ToString()
