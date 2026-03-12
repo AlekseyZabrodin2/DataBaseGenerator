734b5510d946e15079041ff722e04b0485b3dc12
ADANI\zabrodin <rediskal50@gmail.com>
2026-03-12 16:10:35 +0300

    feat: create Mappers

---
+++ DataBaseGenerator.Core/LiteDbGenerator/Mappings/ObjectIdMapper.cs
@@ -0,0 +1,29 @@
+﻿using LiteDB;
+
+namespace DataBaseGenerator.Core.LiteDbGenerator.Mappings
+{
+    internal static class ObjectIdMapper
+    {
+        public static bool TryParse(string? value, out ObjectId id)
+        {
+            id = ObjectId.Empty;
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            try
+            {
+                id = new ObjectId(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static string? ToString(ObjectId id)
+        {
+            return id == ObjectId.Empty ? null : id.ToString();
+        }
+    }
+}
---
+++ DataBaseGenerator.Core/LiteDbGenerator/Mappings/PatientMapper.cs
@@ -0,0 +1,48 @@
+﻿using DataBaseGenerator.Core.LiteDbGenerator.LiteDbModels;
+using DataBaseGenerator.Core.LiteDbGenerator.Models;
+
+namespace DataBaseGenerator.Core.LiteDbGenerator.Mappings
+{
+    public static class PatientMapper
+    {
+        public static LitePatient ToLite(PatientLiteDb patient)
+        {
+            var entity = new LitePatient
+            {
+                PatientId = patient.PatientID,
+                LastName = patient.LastName,
+                FirstName = patient.FirstName,
+                MiddleName = patient.MiddleName,
+                Sex = patient.Sex,
+                BirthDate = patient.BirthDate,
+                Address = patient.Address,
+                Phone = patient.Phone,
+                Comments = patient.Comments
+            };
+
+            if (ObjectIdMapper.TryParse(patient.Id, out var objectId))
+            {
+                entity.Id = objectId;
+            }
+
+            return entity;
+        }
+
+        public static PatientLiteDb ToDomain(LitePatient entity)
+        {
+            return new PatientLiteDb
+            {
+                Id = ObjectIdMapper.ToString(entity.Id),
+                PatientID = entity.PatientId,
+                LastName = entity.LastName,
+                FirstName = entity.FirstName,
+                MiddleName = entity.MiddleName,
+                Sex = entity.Sex,
+                BirthDate = entity.BirthDate,
+                Address = entity.Address,
+                Phone = entity.Phone,
+                Comments = entity.Comments
+            };
+        }
+    }
+}
