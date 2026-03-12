2026464f1f356828b70535da50ea5d8aaf1df9d5
ADANI\zabrodin <rediskal50@gmail.com>
2026-03-12 16:01:32 +0300

    feat: create Patient LiteDb Table UserControl

---
+++ DataBaseGenerator/UserControls/PatientLiteDbTableUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DataBaseGenerator.UI.Wpf.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PatientLiteDbTableUserControl.xaml
+    /// </summary>
+    public partial class PatientLiteDbTableUserControl : UserControl
+    {
+        public PatientLiteDbTableUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
