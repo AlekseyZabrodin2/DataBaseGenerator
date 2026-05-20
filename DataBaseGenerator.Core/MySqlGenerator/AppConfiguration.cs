c9e4a7c353a8b965992ceef93e8f99ab5cda9207
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2026-05-20 11:49:15 +0300

    ref: add method for loading web config, add logger

--- DataBaseGenerator.Core/MySqlGenerator/AppConfiguration.cs
+++ DataBaseGenerator.Core/MySqlGenerator/AppConfiguration.cs
@@ -1,23 +1,76 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using NLog;
 
 namespace DataBaseGenerator.Core.MySqlGenerator
 {
     public static class AppConfiguration
     {
-        private static readonly string ConfigDirectory = Path.Combine(AppContext.BaseDirectory,
-        "../../../AppData");
+        private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        private static readonly string _configDirectory;
+
+        private static readonly string _webConfigDirectory;
+
+
+        static AppConfiguration()
+        {
+#if DEBUG
+
+            _configDirectory = Path.Combine(AppContext.BaseDirectory,"../../../AppData");
+#else
+            
+            _configDirectory = Path.Combine(AppContext.BaseDirectory,"AppData");
+#endif
+            
+            _webConfigDirectory = Path.Combine(AppContext.BaseDirectory,"../AppData");
+        }
 
 
         public static IConfigurationRoot LoadConfiguration()
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-            var configPath = Path.Combine(ConfigDirectory, $"appsettings.{environment}.json");
+            var configPath = Path.Combine(_configDirectory, $"appsettings.{environment}.json");
 
+            _logger.Info($"ENVIRONMENT = {environment}");
+            _logger.Info($"Config path = {configPath}");
+            _logger.Info($"File exists = {File.Exists(configPath)}");
 
-            if (!Directory.Exists(ConfigDirectory))
-                Directory.CreateDirectory(ConfigDirectory);
+            if (File.Exists(configPath))
+            {
+                var content = File.ReadAllText(configPath);
+                _logger.Info($"File content: {content}");
+            }
+
+            if (!Directory.Exists(_configDirectory))
+                Directory.CreateDirectory(_configDirectory);
+
+            if (!File.Exists(configPath))
+                CreateDefaultConfig(configPath);
+
+            return new ConfigurationBuilder()
+                .AddJsonFile(configPath, optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        public static IConfigurationRoot LoadWebConfiguration()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var configPath = Path.Combine(_webConfigDirectory, $"appsettings.{environment}.json");
+
+            _logger.Info($"ENVIRONMENT = {environment}");
+            _logger.Info($"Config path = {configPath}");
+            _logger.Info($"File exists = {File.Exists(configPath)}");
+
+            if (File.Exists(configPath))
+            {
+                var content = File.ReadAllText(configPath);
+                _logger.Info($"File content: {content}");
+            }
+
+            if (!Directory.Exists(_webConfigDirectory))
+                Directory.CreateDirectory(_webConfigDirectory);
 
             if (!File.Exists(configPath))
                 CreateDefaultConfig(configPath);
