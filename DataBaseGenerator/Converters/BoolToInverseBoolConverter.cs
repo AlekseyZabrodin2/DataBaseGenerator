93bbd990fc0a8c7d5c1b8e5c4612a668ffe40faa
ADANI\zabrodin <rediskal50@gmail.com>
2026-06-19 12:45:45 +0300

    ref: create converter

---
+++ DataBaseGenerator/Converters/BoolToInverseBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DataBaseGenerator.UI.Wpf.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class BoolToInverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return InvertBool(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return InvertBool(value);
+        }
+
+
+        private bool InvertBool(object value)
+        {
+            return !(value != null && (bool)value);
+        }
+    }
+}
