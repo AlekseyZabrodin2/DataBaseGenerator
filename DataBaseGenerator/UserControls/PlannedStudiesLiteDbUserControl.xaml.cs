93646f72bab3dfc032748f5490612ce1ccd9bd16
ADANI\zabrodin <rediskal50@gmail.com>
2026-06-26 11:23:49 +0300

    feat: create UserControl for PlannedStudies

---
+++ DataBaseGenerator/UserControls/PlannedStudiesLiteDbUserControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using DataBaseGenerator.UI.Wpf.ViewModel;
+
+namespace DataBaseGenerator.UI.Wpf.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PlannedStudiesLiteDbUserControl.xaml
+    /// </summary>
+    public partial class PlannedStudiesLiteDbUserControl : UserControl
+    {
+        public LiteDbPlannedStudiesViewModel ViewModel { get; private set; }
+
+        public PlannedStudiesLiteDbUserControl()
+        {
+            ViewModel = App.GetService<LiteDbPlannedStudiesViewModel>();
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+    }
+}
